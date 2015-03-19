2e10167ce4256a21e0cdd6e7edba49f3026c1066
JEHR <jehr@dr.dk>
2015-03-19 13:42:56 +0100

    Add Apple II font. Tweak styles and add animations.

--- index.php
+++ index.php
@@ -11,26 +11,34 @@
   </head>
 
   <body>
-    <div class="bg-overlay">
+    <div class="overlay">
       <iframe width="100%" height="100%" src="https://www.youtube.com/embed/-7HQrnlSeRA?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1&amp;modestbranding=1&amp;wmode=transparent&amp;enablejsapi=1" frameborder="0" allowfullscreen></iframe>
       <div class="shield"></div>
     </div>
 
     <div class="wrapper">
-      <div class="top">
+      <div class="header">
         <h1>DYNATRON</h1>
         <p class="lead"><em>Electronic Synth inspired by 80's atmosphere.</em></p>        
       </div>
-      <div class="middle">
+      <div class="body">
       </div>
-      <div class="bottom">
-        <ul class="nav">
-          <li><a href="#">Aeternus</a></li>
-          <li><a href="#">Throttle Up EP</a></li>
-          <li><a href="#">Flashbacks EP</a></li>
-          <li><a href="#">Escape Velocity</a></li>
-          <li><a href="#">Fireburner EP</a></li>
-        </ul>
+      <div class="footer">
+        <div class="nav">
+          <ul class="upper">
+            <li><a href="#">Aeternus</a></li>
+            <li><a href="#">Throttle Up EP</a></li>
+            <li><a href="#">Flashbacks EP</a></li>
+            <li><a href="#">Escape Velocity</a></li>
+            <li><a href="#">Fireburner EP</a></li>
+          </ul>
+          <ul class="lower">
+            <li><a href="https://soundcloud.com/dynatronsynth">Soundcloud</a></li>
+            <li><a href="https://dynatron.bandcamp.com/">Bandcamp</a></li>
+            <li><a href="https://www.facebook.com/dynatronsynth">Facebook</a></li>
+            <li><a href="https://twitter.com/dynatronsynth">Twitter</a></li>
+          </ul>
+        </div>
         <p class="copyright">&copy;2015 DYNATRONSYNTH.COM</p>
       </div>
     </div>
