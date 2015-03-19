1c5062df4620d60995c0be8e131dddaa4baf7242
JEHR <jehr@dr.dk>
2015-03-19 16:08:12 +0100

    Add mobile version

--- m/index.php
+++ m/index.php
@@ -1,19 +1,3 @@
-<?php
-/**
- * Include and instantiate the class.
- */
-require_once 'lib/Mobile-Detect-2.8.12/Mobile_Detect.php';
-$detect = new Mobile_Detect;
-
-// Any mobile device (phones or tablets).
-if ($detect->isMobile()) {
-  header('Location: /m/');
-  die();
-}
-
-?>
-
-
 <!DOCTYPE html>
 <html>
   <head>
@@ -22,22 +6,18 @@ if ($detect->isMobile()) {
     <!-- Viewport mobile tag for sensible mobile support -->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <!--STYLES-->
-    <link rel="stylesheet" href="/styles/style.css">
+    <link rel="stylesheet" href="../styles/style.css">
     <!--STYLES END-->
   </head>
 
-  <body class="desktop">
-    <div class="overlay">
-      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/-7HQrnlSeRA?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1&amp;modestbranding=1&amp;wmode=transparent&amp;enablejsapi=1" frameborder="0" allowfullscreen></iframe>
-      <div class="shield"></div>
-    </div>
-
+  <body class="mobile">
     <div class="wrapper">
       <div class="header">
         <h1>DYNATRON</h1>
         <p class="lead"><em>Electronic Synth inspired by 80's atmosphere.</em></p>        
       </div>
       <div class="body">
+        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/-7HQrnlSeRA?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1&amp;modestbranding=1&amp;wmode=transparent&amp;enablejsapi=1" frameborder="0" allowfullscreen></iframe>
       </div>
       <div class="footer">
         <div class="nav">
@@ -59,7 +39,7 @@ if ($detect->isMobile()) {
       </div>
     </div>
     <!--SCRIPTS-->
-    <script src="/js/main.js"></script>
+    <script src="../js/main.js"></script>
     <!--SCRIPTS END-->
   </body>
 </html>
