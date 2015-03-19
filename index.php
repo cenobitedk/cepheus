929ed8fcf02a429381d728df61057b7d4ff1712e
Jeppe Hasseriis <j.hasseriis@gmail.com>
2015-03-19 07:26:36 +0100

    first steps

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Dynatron</title>
+
+    <!-- Viewport mobile tag for sensible mobile support -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!--STYLES-->
+    <link rel="stylesheet" href="/styles/style.css">
+    <!--STYLES END-->
+  </head>
+
+  <body>
+    <div class="bg-overlay">
+      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/-7HQrnlSeRA?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1&amp;modestbranding=1&amp;wmode=transparent&amp;enablejsapi=1" frameborder="0" allowfullscreen></iframe>
+      <div class="shield"></div>
+    </div>
+
+    <div class="wrapper">
+      <div class="top">
+        <h1>DYNATRON</h1>
+        <p class="lead"><em>Electronic Synth inspired by 80's atmosphere.</em></p>        
+      </div>
+      <div class="middle">
+      </div>
+      <div class="bottom">
+        <ul class="nav">
+          <li><a href="#">Aeternus</a></li>
+          <li><a href="#">Throttle Up EP</a></li>
+          <li><a href="#">Flashbacks EP</a></li>
+          <li><a href="#">Escape Velocity</a></li>
+          <li><a href="#">Fireburner EP</a></li>
+        </ul>
+        <p class="copyright">&copy;2015 DYNATRONSYNTH.COM</p>
+      </div>
+    </div>
+    <!--SCRIPTS-->
+    <script src="/js/main.js"></script>
+    <!--SCRIPTS END-->
+  </body>
+</html>
