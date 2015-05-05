ca2a85e30f2cbb2f9a572dd55e7f35002b4f8d5b
Jeppe Hasseriis <j.hasseriis@gmail.com>
2015-05-05 10:32:25 +0200

    Separate navigation

---
+++ nav.php
@@ -0,0 +1,18 @@
+        <div class="nav">
+          <a href="#menu" class="toggle"><span>Menu</span></a>
+          <ul class="upper">
+            <li><a href="/view/aeternus.html" class="ajax">Aeternus</a></li>
+            <li><a href="/view/throttle-up-ep.html" class="ajax">Throttle Up</a></li>
+            <li><a href="/view/flashbacks-ep.html" class="ajax">Flashbacks</a></li>
+            <li><a href="/view/escape-velocity.html" class="ajax">Escape Velocity</a></li>
+            <li><a href="/view/fireburner-ep.html" class="ajax">Fireburner</a></li>
+          </ul>
+          <ul class="lower">
+            <li><a href="/view/about.html" class="ajax">About</a></li>
+            <li><a href="https://soundcloud.com/dynatronsynth">Soundcloud</a></li>
+            <li><a href="https://dynatron.bandcamp.com/">Bandcamp</a></li>
+            <li><a href="https://www.facebook.com/dynatronsynth">Facebook</a></li>
+            <li><a href="https://twitter.com/dynatronsynth">Twitter</a></li>
+            <li><a href="#" class="mute">Mute</a></li>
+          </ul>
+        </div>
